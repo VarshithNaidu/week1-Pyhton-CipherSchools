e8114bbdf15bffa358cb2b3283ae504d6d5c4080
VarshithNaidu <varshithnaidu28@gmail.com>
2022-11-28 17:28:42 +0530

    print customization
    we can change the outputs of print
    *also changed the wrong name

---
+++ class3-printCommandFunctionalities-CipherSchools.py
@@ -0,0 +1,10 @@
+#print statement concats the strings if they are seperated with ,
+print(1,2,3,4)
+#we can customize the out to come with the 'sep' value
+print(1,2,3,4,5,sep=',')
+#normally the print statement ends with '\n'
+print("Hello")
+print("World")
+#we can customize that by using 'end'
+print("Hello", end =" ")
+print("World")
