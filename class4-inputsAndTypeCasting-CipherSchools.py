6c57f9641e78f560491b4da238d936f4aa513642
VarshithNaidu <varshithnaidu28@gmail.com>
2022-11-28 17:48:12 +0530

    Inputs and typeCasting
    how to take inputs
    and how to type cast in python

---
+++ class4-inputsAndTypeCasting-CipherSchools.py
@@ -0,0 +1,8 @@
+#we can take inputs using the below syntax
+a = input()
+print(a)
+#the type of output of 'print' is a string
+print(type(a))
+#we can coustamize the output by using typecasting
+b = int(input()) # here it explicitsly take only ints
+print(type(b))
