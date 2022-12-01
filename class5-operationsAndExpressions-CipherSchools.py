307dc567ef1005fda80b63ab6313c9274da6a3c5
VarshithNaidu <varshithnaidu28@gmail.com>
2022-12-01 18:43:05 +0530

    operators

---
+++ class5-operationsAndExpressions-CipherSchools.py
@@ -0,0 +1,16 @@
+#operators are [+,*,/,-,%,//,**]
+#operands are variables or constants on which the qperands are used
+#expressions are mixture of oprations and operands
+print(5+5)
+print(10/3)
+#if we add strings we get a concatinated string
+print("hello " + "world")
+#if we multiply a string with number we get those number of concatinated string
+print("hello"*5)
+#comparission operators are used to compare two things
+print(1==2)
+print(1!=2)
+print(2>3)
+print(2>=2)
+#Logical Operators [not,and,or]
+print(True and False)
